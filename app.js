b3834fea9607ff09b4a4777b4dd6d79a8fea017e
janitha thalagoda <112193392+janitha1205@users.noreply.github.com>
2023-10-02 16:48:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const readline = require('readline')
+const express = require('express')
 const { generateMeta,generateImage } = require('./controllers/openaiController')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-//rl.question("YouTube Video Title: \n", (title) => generateMeta(title))
-
-rl.question("Thumbnail description: \n", (desc) => generateImage(desc))
+const app = express()
+app.listen(4000, () => console.log('listening for request on port 4000'))
+//midle weare
+app.use(express.json())
+app.use(express.static('public'))
+//routes
+app.post('/openai/meta',generateMeta)
+app.post('/openai/image',generateImage)
